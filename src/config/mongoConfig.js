988dfab3f90a60f0e981117d0a931fa6d997c89a
trangiabao2702 <meikaruku@gmail.com>
2023-07-03 00:51:03 +0700

    fix mongo connection

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -1,4 +1,4 @@
-export const mongoURI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@zu2702.gtzrpwg.mongodb.net/`;
+export const mongoURI = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@zu2702.gtzrpwg.mongodb.net/${process.env.DATABASE_NAME}`;
 
 export const mongoOptions = {
   useNewUrlParser: true,
